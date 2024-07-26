4699674ac18fb65f1f46951e737903bd44567b1a
Mark Adreian Ramos <121351652+markRamos1974@users.noreply.github.com>
2024-07-26 12:31:59 +0800

    reducing delay per request improving page load time 0.4

--- app/Utilities/FetchAnime.tsx
+++ app/Utilities/FetchAnime.tsx
@@ -1,69 +1,79 @@
-const BASE_URL = 'https://api.jikan.moe/v4'
-
 enum Filter {
-    AIRING = "airing",
-    UPCOMING = "upcoming",
-    BY_POPULARITY = "bypopularity",
-    FAVORITE= "favorite"
+  AIRING = "airing",
+  UPCOMING = "upcoming",
+  BY_POPULARITY = "bypopularity",
+  FAVORITE = "favorite",
 }
 
 enum Rating {
-    ALL_AGES = "g",
-    CHIDLREN = "pg",
-    TEENS = "pg13",
-    VIOLENCE_PROFANITY = "r17",
-    NUDITY = "r",
-    HENTAI = "rx"
+  ALL_AGES = "g",
+  CHIDLREN = "pg",
+  TEENS = "pg13",
+  VIOLENCE_PROFANITY = "r17",
+  NUDITY = "r",
+  HENTAI = "rx",
 }
 
 interface Params {
-    type?:    string,
-    filter?:  Filter,
-    rating?:  Rating,
-    sfw?:     boolean,
-    page?:    number,
-    limit?:   number
+  type?: string;
+  filter?: Filter;
+  rating?: Rating;
+  sfw?: boolean;
+  page?: number;
+  limit?: number;
 }
 
 enum ResponseStatus {
-    BadRequest = 400,
-    NotFound = 404,
-    MethodNotAllowed = 405,
-    TooManyRequest = 429,
-    InternalServerError = 500,
-    ServiceUnavailable = 503
+  BadRequest = 400,
+  NotFound = 404,
+  MethodNotAllowed = 405,
+  TooManyRequest = 429,
+  InternalServerError = 500,
+  ServiceUnavailable = 503,
 }
+
+const BASE_URL = "https://api.jikan.moe/v4";
 
 const extractParams = (params: Params | undefined): string => {
-    if (params === undefined) return ''
-    else {
-        let result: string = "?"
-        Object.entries(params).map(([key, value], index) => {
-            result += `${key}=${value}${index !== (Object.entries(params).length - 1) ? "&" : ""}`
-            
-        })
-        return result
-    }  
-}
+  if (params === undefined) return "";
+  else {
+    let result: string = "?";
+    Object.entries(params).map(([key, value], index) => {
+      result += `${key}=${value}${
+        index !== Object.entries(params).length - 1 ? "&" : ""
+      }`;
+    });
+    return result;
+  }
+};
 
-export async function FetchAnime <T>(endpoint: string, index?: number, params?: Params, ): Promise<T> {
-    //This is to ensure that the request sent to JikanAPI does not reach or exceeds the rate limit
-    //This may cause slow performance since I'm limiting request sent to the API and prevent any runtime error at most
-    const delay = 400
-    const delayInMilis = index ? delay * index : 0
+export async function FetchAnime<T>(
+  endpoint: string,
+  index?: number,
+  params?: Params
+): Promise<T> {
+  //This is to ensure that the request sent to JikanAPI does not reach or exceeds the rate limit
+  //This may cause slow performance since I'm limiting request sent to the API and prevent any runtime error at most
+  const delay = 300;
+  const delayInMilis = index ? delay * index : 0;
 
-    return new Promise((resolve => setTimeout(resolve, delayInMilis))).then(async() => {
-        
-        const parameters = extractParams(params ? params : undefined)
-        const url = `${BASE_URL}${endpoint}${parameters ? parameters : ''}`
-        const response = await fetch(url, {
-            method: 'GET'
-        })
+  return new Promise((resolve) => setTimeout(resolve, delayInMilis)).then(
+    async () => {
 
-        if(response.status === ResponseStatus.TooManyRequest) throw Error("Too many request! rate limit exceed")
-        console.log(`\nEndpoint : ${endpoint}\nResponse Status: ${response.status}\nDelay: ${delayInMilis}`)
-        
-        const result : T = await response.json()
-        return result
-    })
+      const parameters = extractParams(params ? params : undefined);
+      const url = `${BASE_URL}${endpoint}${parameters ? parameters : ""}`;
+      const response = await fetch(url, {
+        method: "GET",
+      });
+
+      if (response.status === ResponseStatus.TooManyRequest)
+        throw Error("Too many request! rate limit exceed");
+
+      console.log(
+        `\nEndpoint : ${endpoint}\nResponse Status: ${response.status}\nDelay: ${delayInMilis}`
+      );
+      const result: T = await response.json();
+      return result;
+    }
+  );
 }
