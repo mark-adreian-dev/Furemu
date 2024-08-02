6341e31e3d37bdaf9c6d61fb20f5079cb11c5403
Mark Adreian Ramos <121351652+markRamos1974@users.noreply.github.com>
2024-08-02 18:00:50 +0800

    fixing loading screen hierarchy

--- app/Components/SearchPageComponents/SearchCard.tsx
+++ app/Components/SearchPageComponents/SearchCard.tsx
@@ -15,7 +15,7 @@ const SearchCard:React.FC<Props> = ({ data, isLoading, type }) => {
   if(isLoading) {
     return(
       <div className="h-[calc(100vh-18rem)] flex flex-col justify-center items-center">
-        <span className="w-32 h-32 loading loading-ring mb-2 block"></span>
+        <span className="w-32 h-32 loading loading-ring mb-2 block bg-white"></span>
         <p className="text-white italic">Searching results...</p>
       </div>
     )
@@ -26,7 +26,7 @@ const SearchCard:React.FC<Props> = ({ data, isLoading, type }) => {
       <div className="card-container grid grid-cols-2 gap-x-2 gap-y-6 tablet:grid-cols-3 tablet:gap-y-8 tablet:gap-x-4 mt-8 desktop:grid-cols-5 mb-16">
         {
           data.map((item) => (
-            <Link key={item.mal_id} href={`/${type}/${item.mal_id}`} scroll={false}>
+            <a key={item.mal_id} href={`/${type}/${item.mal_id}`}>
               <div >
                 <div className="relative rounded-md overflow-hidden h-[calc(((100vw-3rem-0.5rem)/2)*1.406249338397503)] tablet:h-[calc(((100vw-3rem-2rem)/3)*1.406249338397503)] tablet:rounded-2xl desktop:tablet:h-[calc((((100vw-8rem-14.3125rem)-3rem-4rem)/5)*1.406248585365854)] desktop:rounded-3xl ">
                   <Image
@@ -44,7 +44,7 @@ const SearchCard:React.FC<Props> = ({ data, isLoading, type }) => {
                   {item.title}
                 </h4>
               </div>
-            </Link>
+            </a>
           ))
         }
       </div>
