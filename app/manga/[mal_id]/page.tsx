5a5ca3b0d13825cda55f198aecd68abb7efc5262
Mark Adreian Ramos <121351652+markRamos1974@users.noreply.github.com>
2024-08-02 14:45:30 +0800

    optimizing images for faster load time 0.4

--- app/manga/[mal_id]/page.tsx
+++ app/manga/[mal_id]/page.tsx
@@ -2,20 +2,21 @@ import Header from "@/app/Components/Header";
 import AnimePreview from "../../Components/AnimePreviewComponents/AnimePreview";
 import SmoothScroll from "@/app/Components/SmoothScroll";
 
+import { Anime } from "@/app/Types/Anime";
 import { FetchAnime } from "@/app/Utilities/FetchAnime";
 import { Type } from "@/app/Types/Enums"
 import { Manga } from "@/app/Types/Manga";
 
 const page = async ({ params }: { params: { mal_id: string } }) => {
   const animeId = params.mal_id;
-  const endpoint = `/manga/${animeId}/full`
+  const endpoint= `/manga/${animeId}/full`
   const mangaData: Manga = await FetchAnime(endpoint);
 
   return (
     <>
       <SmoothScroll root={true}>
         <Header />
-        <AnimePreview mangaData={mangaData.data} type={Type.manga}/>
+        <AnimePreview data={(mangaData as Manga).data} type={Type.manga}/>
       </SmoothScroll>
     </>
   );
